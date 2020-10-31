e96b1a0d8cd248d9ae2de8db96582180382b4045
yuta0326 <72392199+yuta0326@users.noreply.github.com>
2020-10-31 19:49:26 +0900

    課題提出テスト
    Task1_5.java の作成

---
+++ JavaStudy/1-5/test.java
@@ -0,0 +1,23 @@
+
+
+public class test{
+
+    public static void main(String[] args){
+// int配列の宣言と初期化 
+int[] arr = {5, 8, 2, 6}; 
+
+// 配列arrの中身を全て表示する 
+for (int i = 0; i < arr.length; i++) { 
+    System.out.println("arr[" + i + "] … " + arr[i]); 
+
+
+
+
+    
+}
+
+
+    }
+
+
+}
