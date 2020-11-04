7f187128e5b8c1c26ae9b22415ce970a30262b66
yuta0326 <72392199+yuta0326@users.noreply.github.com>
2020-11-04 21:24:38 +0900

    課題提出テスト
    Task1_5の作成

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -37,13 +37,21 @@ public class Task1_4 {
         if ( name.equals("alice") && pass.equals("pass")) {
             System.out.println(CONST_MSG_SUCCESS);
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        } else if(name.equals("alice"));
+        } else if(name.equals("alice")){
+            System.out.println(CONST_MSG_ERROR_PASS);
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-          else if(pass.equals("pass"));
+        }else if(pass.equals("pass")){
+            System.out.println(CONST_MSG_ERROR_NAME );
 
+    
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
-        else
+        }else{
+            System.out.println(CONST_MSG_ERROR_INPUT);
+        }
+
     }
+
+
 
 }
